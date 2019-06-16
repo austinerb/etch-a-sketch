2c507ab64df702a6b23e38c0942a6afb574a65df
Austin Erb <austin.erb8@gmail.com>
2019-06-16 19:49:27 -0400

    Initialize repository

---
+++ script.js
@@ -0,0 +1,13 @@
+// get container and button
+// dimension = 16
+// function create (dimension)
+    //loop through dimensions
+        // create a div
+        // add hover event listener to div
+            // if background is white: change to black
+            // else: return 
+        // add div to container
+// add click event listener to button
+    // remove all children from container
+    // prompt for new dimensions
+    // create(new dimension)
